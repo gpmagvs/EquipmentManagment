7a39e467d74a140a56592a63759eae81335cc14d
jinweihuang20 <jinweihuang1020@gmail.com>
2024-06-08 15:21:35 +0800

    修改 clsPortOfRack.cs/ 新增EventHandler

--- Device/PortStatusAbstract.cs
+++ Device/PortStatusAbstract.cs
@@ -6,10 +6,25 @@ namespace EquipmentManagment.Device
 {
     public abstract class PortStatusAbstract
     {
-        public string CarrierID { get; set; }
+
+        public static event EventHandler<string> CarrierIDChanged;
+
+        private string _CarrierID = "";
+        public string CarrierID
+        {
+            get => _CarrierID;
+            set
+            {
+                if (_CarrierID != value)
+                {
+                    _CarrierID = value;
+                    CarrierIDChanged?.Invoke(this, value);
+                }
+            }
+        }
         public bool CarrierExist { get; set; }
         public DateTime InstallTime { get; set; } = DateTime.MinValue;
     }
 
-    
+
 }
--- WIP/clsPortOfRack.cs
+++ WIP/clsPortOfRack.cs
@@ -75,9 +75,6 @@ namespace EquipmentManagment.WIP
         public static event EventHandler<(clsRack rack, clsPortOfRack port)> OnRackPortSensorFlash;
         public static event EventHandler<(clsRack rack, clsPortOfRack port)> OnRackPortSensorStatusChanged;
 
-        [NonSerialized]
-        public ConcurrentQueue<Dictionary<SENSOR_LOCATION, bool>> QueExistSensorStates = new ConcurrentQueue<Dictionary<SENSOR_LOCATION, bool>>();
-
         public Dictionary<SENSOR_LOCATION, SENSOR_STATUS> ExistSensorStates { get; set; } = new Dictionary<SENSOR_LOCATION, SENSOR_STATUS>()
         {
             { SENSOR_LOCATION.TRAY_1 ,SENSOR_STATUS.OFF },
@@ -126,87 +123,8 @@ namespace EquipmentManagment.WIP
                 return CARGO_PLACEMENT_STATUS.NO_CARGO_BUT_CLICK;
             else
                 return CARGO_PLACEMENT_STATUS.NO_CARGO;
-
-
-            //if (sensor1 && sensor2)
-            //    return CARGO_PLACEMENT_STATUS.PLACED_NORMAL;
-            //else if ((sensor1 && !sensor2) || (!sensor1 && sensor2))
-            //    return CARGO_PLACEMENT_STATUS.PLACED_BUT_ASYMMETRIC;
-            //else if (NO_CARGO_BUT_CLICK)
-            //    return CARGO_PLACEMENT_STATUS.NO_CARGO_BUT_CLICK;
-            //else
-            //    return CARGO_PLACEMENT_STATUS.NO_CARGO;
         }
 
-        private Dictionary<SENSOR_LOCATION, bool> temp_statuscounter = new Dictionary<SENSOR_LOCATION, bool>(){
-                            { SENSOR_LOCATION.TRAY_1 ,false },
-                            { SENSOR_LOCATION.TRAY_2 ,false },
-                            { SENSOR_LOCATION.RACK_1 ,false },
-                            { SENSOR_LOCATION.RACK_2 ,false },
-        };
-
-        private void CheckSensorClick2()
-        {
-            if (DateTime.Now.Second % 5 == 0)
-            {
-                try
-                {
-                    Dictionary<SENSOR_LOCATION, bool> finall_statuscounter = new Dictionary<SENSOR_LOCATION, bool>(){
-                            { SENSOR_LOCATION.TRAY_1 ,false },
-                            { SENSOR_LOCATION.TRAY_2 ,false },
-                            { SENSOR_LOCATION.RACK_1 ,false },
-                            { SENSOR_LOCATION.RACK_2 ,false },
-                        };
-                    Dictionary<SENSOR_LOCATION, int[]> statuscounter = new Dictionary<SENSOR_LOCATION, int[]>(){
-                            { SENSOR_LOCATION.TRAY_1 ,new int[]{0,0,0 } },
-                            { SENSOR_LOCATION.TRAY_2 ,new int[]{0,0,0 } },
-                            { SENSOR_LOCATION.RACK_1 ,new int[]{0,0,0 } },
-                            { SENSOR_LOCATION.RACK_2 ,new int[]{0,0,0 } },
-                        };
-                    int count = 0;
-                    int flashcount = 0;
-                    while (QueExistSensorStates.TryDequeue(out Dictionary<SENSOR_LOCATION, bool> temp_ExistSensorStates))
-                    {
-                        foreach (var item in temp_ExistSensorStates)
-                        {
-                            statuscounter[item.Key][0]++;
-                            if (item.Value != temp_statuscounter[item.Key])
-                            {
-                                finall_statuscounter[item.Key] = temp_ExistSensorStates[item.Key];
-
-                                flashcount += 1;
-                            }
-                            if (temp_ExistSensorStates[item.Key] == true)
-                                statuscounter[item.Key][1]++;
-                            else
-                                statuscounter[item.Key][2]++;
-
-
-                            count += 1;
-                        }
-
-                    }
-                    foreach (var item in finall_statuscounter)
-                    {
-                        if ((statuscounter[SENSOR_LOCATION.TRAY_1][0] == statuscounter[SENSOR_LOCATION.TRAY_1][1] && statuscounter[SENSOR_LOCATION.TRAY_2][0] == statuscounter[SENSOR_LOCATION.TRAY_2][1]) || (statuscounter[SENSOR_LOCATION.RACK_1][0] == statuscounter[SENSOR_LOCATION.RACK_1][1] && statuscounter[SENSOR_LOCATION.RACK_2][0] == statuscounter[SENSOR_LOCATION.RACK_2][1]))
-                            ExistSensorStates[item.Key] = SENSOR_STATUS.ON;
-                        if ((statuscounter[SENSOR_LOCATION.TRAY_1][0] == statuscounter[SENSOR_LOCATION.TRAY_1][2] && statuscounter[SENSOR_LOCATION.TRAY_2][0] == statuscounter[SENSOR_LOCATION.TRAY_2][2]) || (statuscounter[SENSOR_LOCATION.RACK_1][0] == statuscounter[SENSOR_LOCATION.RACK_1][2] && statuscounter[SENSOR_LOCATION.RACK_2][0] == statuscounter[SENSOR_LOCATION.RACK_2][2]))
-                            ExistSensorStates[item.Key] = SENSOR_STATUS.OFF;
-                        else if (flashcount >= count / 2)
-                            ExistSensorStates[item.Key] = SENSOR_STATUS.FLASH;
-                    }
-
-                    //foreach (var item in ExistSensorStates)
-                    //{
-                    //    ExistSensorStates[item.Key] = current_ExistSensorStates[item.Key] == true ? SENSOR_STATUS.ON : SENSOR_STATUS.OFF;
-                    //}
-                }
-                catch (Exception ex)
-                {
-                    throw;
-                }
-            }
-        }
         Dictionary<SENSOR_LOCATION, bool> current_ExistSensorStates = new Dictionary<SENSOR_LOCATION, bool>()
                 {
                     { SENSOR_LOCATION.TRAY_1 ,true },
@@ -214,7 +132,6 @@ namespace EquipmentManagment.WIP
                     { SENSOR_LOCATION.RACK_1 ,true },
                     { SENSOR_LOCATION.RACK_2 ,true },
                 };
-        Dictionary<SENSOR_LOCATION, bool> _previousSensorStates = new Dictionary<SENSOR_LOCATION, bool>();
         internal void UpdateIO(ref bool[] inputBuffer)
         {
             clsRackPortProperty.clsPortIOLocation ioLocation = Properties.IOLocation;
@@ -233,23 +150,15 @@ namespace EquipmentManagment.WIP
                 Console.WriteLine($"clsWIPPort -> UpdateIO From inputs buffer Fail: {ex.Message}");
             }
 
-            async void HandleSensorStatus(bool[] _inputBuffer, SENSOR_LOCATION _sensorLocation, ushort _ioLocation)
+            void HandleSensorStatus(bool[] _inputBuffer, SENSOR_LOCATION _sensorLocation, ushort _ioLocation)
             {
                 bool currentSensorStatus = _inputBuffer[_ioLocation];
 
                 if (current_ExistSensorStates[_sensorLocation] != currentSensorStatus)
                 {
                     CancellationTokenSource _cts = _StatusDelayCancellationTks[_sensorLocation];
-                    if (_cts != null)
-                    {
-                        _cts.Cancel();
-                        await Task.Delay(100);
-                        SensorStatusChangedDelayAsync(_sensorLocation, currentSensorStatus);
-                    }
-                    else
-                    {
-                        SensorStatusChangedDelayAsync(_sensorLocation, currentSensorStatus);
-                    }
+                    _cts?.Cancel();
+                    SensorStatusChangedDelayAsync(_sensorLocation, currentSensorStatus);
                     current_ExistSensorStates[_sensorLocation] = currentSensorStatus;
                 }
             }
@@ -257,10 +166,10 @@ namespace EquipmentManagment.WIP
         }
         Dictionary<SENSOR_LOCATION, CancellationTokenSource> _StatusDelayCancellationTks = new Dictionary<SENSOR_LOCATION, CancellationTokenSource>()
         {
-            { SENSOR_LOCATION.TRAY_1  ,  null},
-            { SENSOR_LOCATION.TRAY_2  ,  null},
-            { SENSOR_LOCATION.RACK_1  ,  null},
-            { SENSOR_LOCATION.RACK_2  ,  null},
+            { SENSOR_LOCATION.TRAY_1  ,  new CancellationTokenSource()},
+            { SENSOR_LOCATION.TRAY_2  ,  new CancellationTokenSource()},
+            { SENSOR_LOCATION.RACK_1  ,  new CancellationTokenSource()},
+            { SENSOR_LOCATION.RACK_2  ,  new CancellationTokenSource()},
         };
 
         public DateTime timestamp { get; set; } = DateTime.MinValue;
@@ -269,23 +178,22 @@ namespace EquipmentManagment.WIP
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
             _StatusDelayCancellationTks[location] = new CancellationTokenSource();
-            while (stopwatch.ElapsedMilliseconds < 700)
+            try
             {
-                if (_StatusDelayCancellationTks[location].IsCancellationRequested)
-                {
-                    stopwatch.Stop();
-                    var previosState = ExistSensorStates[location];
-
-                    if (previosState != SENSOR_STATUS.FLASH)
-                    {
-                        ExistSensorStates[location] = SENSOR_STATUS.FLASH;
-                        OnRackPortSensorFlash?.Invoke(this, (ParentRack, this));
-                    }
-                    Console.WriteLine($"{this.ParentRack.EQName}-Port [{this.Properties.ID}]-{location} chagne to {ExistSensorStates[location]}");
-                    return;
-                }
-                await Task.Delay(1);
+                await Task.Delay(700, _StatusDelayCancellationTks[location].Token);
             }
+            catch (TaskCanceledException)
+            {
+                var previosState = ExistSensorStates[location];
+                if (previosState != SENSOR_STATUS.FLASH)
+                {
+                    ExistSensorStates[location] = SENSOR_STATUS.FLASH;
+                    OnRackPortSensorFlash?.Invoke(this, (ParentRack, this));
+                }
+                Console.WriteLine($"{this.ParentRack.EQName}-Port [{this.Properties.ID}]-{location} chagne to {ExistSensorStates[location]}");
+                return;
+            }
+
             ExistSensorStates[location] = currentSatus ? SENSOR_STATUS.OFF : SENSOR_STATUS.ON;
             OnRackPortSensorStatusChanged?.Invoke(this, (ParentRack, this));
             Console.WriteLine($"{this.ParentRack.EQName}-Port [{this.Properties.ID}]-{location} chagne to {ExistSensorStates[location]}");
