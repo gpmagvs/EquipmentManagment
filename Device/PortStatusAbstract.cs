fec00e2dd73af7a525388701eb76df388479b9a3
jinweihuang20 <jinweihuang1020@gmail.com>
2024-12-06 17:49:37 +0800

    更新 PortStatusAbstract

--- Device/PortStatusAbstract.cs
+++ Device/PortStatusAbstract.cs
@@ -8,7 +8,7 @@ namespace EquipmentManagment.Device
 {
     public abstract class PortStatusAbstract
     {
-        public static event EventHandler<(string newValue, string oldValue, bool isUpdateByVehicleLoadTo)> CarrierIDChanged;
+        public static event EventHandler<(string newValue, string oldValue, bool isUpdateByVehicleLoadUnLoad)> CarrierIDChanged;
 
         public string NickName { get; set; } = "TEST";
 
@@ -25,7 +25,7 @@ namespace EquipmentManagment.Device
 
                     if (GetType().Name == typeof(clsPortOfRack).Name)
                     {
-                        CarrierIDChanged?.Invoke(this, (value, oldValue, VehicleLoadToPortFlag));
+                        CarrierIDChanged?.Invoke(this, (value, oldValue, VehicleLoadToPortFlag || VehicleUnLoadFromPortFlag));
                     }
                 }
             }
@@ -36,6 +36,7 @@ namespace EquipmentManagment.Device
         public RACK_CONTENT_STATE RackContentState { get; set; } = RACK_CONTENT_STATE.UNKNOWN;
 
         public bool VehicleLoadToPortFlag { get; set; } = false;
+        public bool VehicleUnLoadFromPortFlag { get; set; } = false;
     }
 
 
